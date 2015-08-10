132654bb9080602c4c256ffa4b3188a2adf93e55
Mark Keysor <markkeysor@gmail.com>
2015-08-10 13:24:06 -0600

    setting up twillio

--- server.js
+++ server.js
@@ -2,11 +2,32 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+var request = require('request');
 
 var app = express();
 var port = 3030;
 
+///////// MIDDLEWARE ///////////////////////
 
+app.use(express.static('public'));
+app.use(bodyParser());
+app.use(cors());
+
+var message = {
+  "message": "HELLO WORLD!!"
+}
+
+///////// ENDPOINTS ///////////////////////
+
+app.get('/api/message', function(req, res) {
+  return res.json(message);
+})
+
+app.post('/api/send_text_message', function(req, res) {
+  console.log(req.body.message);
+  res.send();
+})
 
 
 app.listen(port, function() {
